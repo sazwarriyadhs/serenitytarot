f7536f626118e2fc6e42e03a65eb322072c30807
Papa Bey <pabeystore@gmail.com>
2025-07-01 14:27:56 +0000

    Gunakan /home/user/studio/public/images/logo.png untuk logo aplikasi

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,19 +1,14 @@
-import type { SVGProps } from "react";
+import Image from 'next/image';
+import type { ComponentProps } from 'react';
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
+export function Logo(props: Omit<ComponentProps<typeof Image>, 'src' | 'alt'>) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+    <Image
+      src="/images/logo.png"
+      alt="Mystic Agenda Logo"
+      width={32}
+      height={32}
       {...props}
-    >
-      <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z" />
-      <path d="M17.5 4.5a2.5 2.5 0 0 1 0 5" />
-    </svg>
+    />
   );
 }
